2762ce8330fbdd6dbcd906b0fa44a6a6b414fd08
JerryAgbo <90285713+JerryAgbo@users.noreply.github.com>
2021-11-22 13:39:49 +0000

    First Commit

---
+++ src/JAVAGUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class JAVAGUI {
+    private JPanel MAIN;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton SEARCHButton;
+}
